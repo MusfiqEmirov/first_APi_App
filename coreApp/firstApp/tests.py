1c1b8027a4e0232955aa8ec59ccf711f71952c25
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-02-17 23:47:50 +0400

    post methodu test edildi

--- coreApp/firstApp/tests.py
+++ coreApp/firstApp/tests.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 # mehsullarin duzgun alinib ve getirlmeyi baresinde test
 class ProductsAPIViewTest(APITestCase):
+    # get methodunun test edilmesi
     def setUp(self):
         # testden evvel bezi mehsullar elave edirkk
         Product.objects.create(name="product name1",price=100)
@@ -24,4 +25,46 @@ class ProductsAPIViewTest(APITestCase):
          self.assertEqual(response.data[0]["name"], "product name1") # bu birinci mehsulun adi
          self.assertEqual(response.data[1]["name"], "product name2") # bu ikinci mehsulun adi
 
-        
+    # post methodunun test edilmesi ve yeni mehsul elave edilmesi
+    def test_post_product(self):
+        url = reverse('products')
+        data = {
+            "name": "balli tort",
+            "price": 5.9,
+            "description":"en dadli tort",
+            "imageUrl":"545.jpg",
+            "isActive": True,
+            "slug": "en-dadli-tort"
+        }
+        #POST sorgusu gonderirlr
+        response = self.client.post(url,data,format="json")
+
+        #status kodunun yoxlanilmasi
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # yeni mehsulun elave dildiyini yoxlamag
+        self.assertEqual(response.data["name"], "balli tort")
+        self.assertEqual(response.data["price"], 5.9)
+
+        # yeni mehsulun data bazaya duwmesi yoxlanilir
+        self.assertEqual(Product.objects.count(), 3) # burda artiq 3 mehsul olamlidir
+
+    def test_post_invalid_product(self):
+        #POST methodu ile qiymetsis gonderilse
+        url = reverse("products")
+        data = {
+            "name": "no validation"
+        }
+
+        # post sorgusu  gonderilir
+        response = self.client.post(url,data,format="json")
+
+        # status kodu yoxlanilir
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # error mesaji yoxlanilir
+        self.assertIn('price', response.data) # teleb olunan sahe
+
+
+
+
