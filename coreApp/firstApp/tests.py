6571a556268fd21f705e9e6e3ba134712a0c5791
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-02-17 23:21:24 +0400

    post method with data create added

--- coreApp/firstApp/tests.py
+++ coreApp/firstApp/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from firstApp.models import Product
+from django.urls import reverse
 
-# Create your tests here.
+
+# mehsullarin duzgun alinib ve getirlmeyi baresinde test
+class ProductsAPIViewTest(APITestCase):
+    def setUp(self):
+        # testden evvel bezi mehsullar elave edirkk
+        Product.objects.create(name="product name1",price=100)
+        Product.objects.create(name="product name2",price=200)
+
+    def test_get_products(self):
+        # get methodu ile mehsullarin duzgun alindigini yoxlayiriq
+         url = reverse('products')  # verdiyimiz endpoit nameni yaziriq
+         response = self.client.get(url)
+
+         #status kodunu yoxlayiriq 200 olamldir
+         self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+         # verilenlerin duzgun oldugunu yoxlamag
+         self.assertEqual(len(response.data),2)  # yeni 2 mehsul olmalidir
+         self.assertEqual(response.data[0]["name"], "product name1") # bu birinci mehsulun adi
+         self.assertEqual(response.data[1]["name"], "product name2") # bu ikinci mehsulun adi
+
+        
--- coreApp/view_manager/views.py
+++ coreApp/view_manager/views.py
@@ -7,8 +7,15 @@ from firstApp.serializers import ProductSerializer
 
 class ProductsAPIView(APIView):
     def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products,many=True)
+        products = Product.objects.all() # butun obyekctleri aliriq
+        serializer = ProductSerializer(products,many=True) # birden daha cox abyekt olacagini qeyd edirik
         return Response(serializer.data)
     
+    def post(self,request):
+        serializer = ProductSerializer(data=request.data) #json melumatlarini evvelce parse edirik
+
+        if serializer.is_valid(): # validation gedir
+            serializer.save() # ve yaddawa yazir
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # ugurlu melyatda 201 status kodu qaytarir ve elave olundu
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # yox eger ugursuzdusa yalniw sorgu olaraq geri qayatrir
 
