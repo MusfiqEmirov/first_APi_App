89f1f0bf465670686a3caaa250a4d1213e1456f6
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-03-18 11:15:46 +0400

    securty createuser for added

--- core_app/core_app/celery.py
+++ core_app/core_app/celery.py
@@ -3,10 +3,13 @@ import os
 from celery import Celery
 
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_app.settings')
 
 app = Celery('core_app')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.broker_connection_retry_on_startup = True
 
 app.autodiscover_tasks()
